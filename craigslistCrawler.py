8a0290d4099cb9e7a03dae84e47d31b4eceebace
Abukasis <AddieAbuka@gmail.com>
2019-10-18 01:57:09 -0700

    Update craigslistCrawler.py
    cleaned up

--- craigslistCrawler.py
+++ craigslistCrawler.py
@@ -1,17 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-url2 = "https://losangeles.craigslist.org/search/jjj?query="
+#reads craigslist postings 
+
 userIn = input("Enter Job Search Criteria:" )
+url2 = "https://losangeles.craigslist.org/search/jjj?query="
 url2 += userIn
+
 file = open("jobLinks.txt","w+")
+
 result = requests.get(url2)
 resultContent = result.content
 soupResult = BeautifulSoup(resultContent,features="html.parser")
+
 totalFound = soupResult.findAll("span",class_= "totalcount")
 totalPages = int(totalFound[0].get_text()) / 120
+
 if (totalPages % 120) != 0:
 	totalPages += 1
+	
 foundLinks = soupResult.findAll("a", class_ = "result-title hdrlnk")
 for link in foundLinks:
 	t = str(link.get_text())
